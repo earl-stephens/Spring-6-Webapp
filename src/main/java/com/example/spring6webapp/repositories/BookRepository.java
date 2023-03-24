ed6d9544beccb059a22922a991970339338d754d
earl-stephens <stephensearl@hotmail.com>
2023-03-24 17:02:26 -0600

    Setup repositories

---
+++ src/main/java/com/example/spring6webapp/repositories/BookRepository.java
@@ -0,0 +1,8 @@
+package com.example.spring6webapp.repositories;
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.spring6webapp.domain.Book;
+
+public interface BookRepository extends CrudRepository<Book, Long> {
+
+}
