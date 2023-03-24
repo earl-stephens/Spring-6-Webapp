9e60391e1b3472ee8d39780765751d62357183be
earl-stephens <stephensearl@hotmail.com>
2023-03-24 16:24:50 -0600

    Start setting up JPA relationships

--- src/main/java/com/example/spring6webapp/domain/Author.java
+++ src/main/java/com/example/spring6webapp/domain/Author.java
@@ -1,9 +1,12 @@
 package com.example.spring6webapp.domain;
 
+import java.util.Set;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 
 @Entity
 public class Author {
@@ -13,4 +16,33 @@ public class Author {
 	private Long id;
 	private String firstName;
 	private String lastName;
+	
+	@ManyToMany(mappedBy = "authors")
+	private Set<Book> books;
+	
+	public Set<Book> getBooks() {
+		return books;
+	}
+	public void setBooks(Set<Book> books) {
+		this.books = books;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 }
