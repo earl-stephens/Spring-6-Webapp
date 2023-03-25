6b45ded68a088508e355ac208b8506f838c049ca
earl-stephens <stephensearl@hotmail.com>
2023-03-25 16:43:56 -0600

    Set up service layer for books

---
+++ src/main/java/com/example/spring6webapp/services/BookServiceImpl.java
@@ -0,0 +1,21 @@
+package com.example.spring6webapp.services;
+
+import org.springframework.stereotype.Service;
+
+import com.example.spring6webapp.domain.Book;
+import com.example.spring6webapp.repositories.BookRepository;
+
+@Service
+public class BookServiceImpl implements BookService {
+	
+	private final BookRepository bookRepository;
+	
+	public BookServiceImpl(BookRepository bookRepository) {
+		this.bookRepository = bookRepository;
+	}
+	
+	@Override
+	public Iterable<Book> findAll() {
+		return bookRepository.findAll();
+	}
+}
